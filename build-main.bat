ad518a9c39c8336fa9fbf816f131bf3a6f5e59dd
Weslei dos Santos <weslei.santos@cybersoftsistemas.com.br>
2025-09-07 19:47:59 -0300

    ...

--- build-main.bat
+++ build-main.bat
@@ -13,14 +13,7 @@ call "C:\Program Files (x86)\Embarcadero\Studio\23.0\bin\rsvars.bat"
 REM ============================
 REM Caminhos
 REM ============================
-set DELPHI_BIN="C:\Program Files (x86)\Embarcadero\Studio\23.0\bin"
 set PROJECT_PATH=C:\actions-runner\_work\duimp\duimp\src\client\Siscomex.groupproj
-
-rem ============================
-rem Variáveis de ambiente Delphi
-rem ============================
-set BDS=C:\Users\Administrator\AppData\Roaming\Embarcadero\BDS\23.0
-set PATH=%PATH%;%DELPHI_BIN%
 
 REM ============================
 REM Compilar Delphi
