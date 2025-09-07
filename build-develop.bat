735e43c900e31f20132329ac3c9e79c561e02a39
Weslei dos Santos <weslei.santos@cybersoftsistemas.com.br>
2025-09-07 19:08:40 -0300

    ...

--- build-develop.bat
+++ build-develop.bat
@@ -18,7 +18,7 @@ set PROJECT_PATH=C:\actions-runner\_work\duimp\duimp\src\client\Siscomex.grouppr
 REM ============================
 REM Compilar Delphi
 REM ============================
-msbuild.exe "%PROJECT_PATH%" /t:Build /p:Config=Release /p:Platform=Win32
+msbuild.exe "%PROJECT_PATH%" /t:Build /p:Config=VCL /p:Platform=Win32
 if errorlevel 1 (
     echo ERRO NA COMPILACAO DO DELPHI
     exit /b 1
