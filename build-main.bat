683bb88f9b7265abf226d51fd55596301a24868f
Weslei dos Santos <weslei.santos@cybersoftsistemas.com.br>
2025-09-07 18:54:51 -0300

    ...

--- build-main.bat
+++ build-main.bat
@@ -6,15 +6,19 @@ echo Compilando Delphi para ambiente: main
 echo ====================================
 
 REM ============================
-REM Caminhos (ajuste conforme sua máquina runner)
+REM Carregar variáveis do Delphi
 REM ============================
-set DELPHI_PATH=C:\Program Files (x86)\Embarcadero\Studio\23.0\bin
+call "C:\Program Files (x86)\Embarcadero\Studio\23.0\bin\rsvars.bat"
+
+REM ============================
+REM Caminhos
+REM ============================
 set PROJECT_PATH=C:\actions-runner\_work\duimp\duimp\src\client\Siscomex.groupproj
 
 REM ============================
 REM Compilar Delphi
 REM ============================
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "%PROJECT_PATH%" /t:Build /p:Config=Release /p:Platform=Win32
+msbuild.exe "%PROJECT_PATH%" /t:Build /p:Config=Release /p:Platform=Win32
 if errorlevel 1 (
     echo ERRO NA COMPILACAO DO DELPHI
     exit /b 1
@@ -23,5 +27,4 @@ if errorlevel 1 (
 echo ====================================
 echo Compilacao concluida com sucesso!
 echo ====================================
-
 exit /b 0
