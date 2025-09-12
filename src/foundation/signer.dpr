724f344933beff51f5f1bea0a1db63bf3d31a902
Weslei dos Santos <weslei.santos@cybersoftsistemas.com.br>
2025-09-11 23:26:18 -0300

    signer: Aceitar diretórios via parâmetro -directories=... Se o usuário passar esse parâmetro, o programa ignora o valor do Directories no .ini. Pode passar múltiplos caminhos separados por ;, tanto absolutos quanto relativos. Exibir apenas caminhos relativos no output baseado no diretório onde está o signer.exe. Os logs e mensagens no console não mostram mais o caminho absoluto. Forçar reassinatura: signer.exe -force yml: O signer.exe com o parâmetro -directories=... para que ele ignore o valor do .ini e use apenas os diretórios informados na execução.

--- src/foundation/signer.dpr
+++ src/foundation/signer.dpr
@@ -34,32 +34,24 @@ begin
   var LBase := TPath.GetFullPath(ABasePath);
   var LFull := TPath.GetFullPath(AFullPath);
 
-  // normaliza para ter "\" no final
   if not LBase.EndsWith(PathDelim) then
     LBase := LBase + PathDelim;
 
   if LFull.StartsWith(LBase, True) then
     Result := '.' + PathDelim + LFull.Substring(LBase.Length)
   else
-    Result := AFullPath; // se não for subpasta, mostra como está
-end;
-
-function ToRelativePath(const ABasePath, AFileName: string): string;
-begin
-  Result := GetRelativePath(ABasePath, AFileName);
-  if Result.StartsWith('..') then
-    Result := AFileName; // fallback se não for possível relativizar
+    Result := AFullPath;
 end;
 
 function FileNeedsSigning(const AFileName, ALogFile: string): Boolean;
 var
   LF: TextFile;
   LLine: string;
-  FileHash: string;
 begin
   Result := True;
   if not FileExists(ALogFile) then Exit;
-  FileHash := THashSHA2.GetHashString(AFileName);
+
+  var FileHash := THashSHA2.GetHashString(AFileName);
   AssignFile(LF, ALogFile);
   Reset(LF);
   try
@@ -77,24 +69,23 @@ begin
   end;
 end;
 
-procedure SignFile(const AFileName, ASigntoolPath, ACertFile, ACertPass, ATimestamp, AHashAlgo, ABasePath: string;
-  const AEnableLog, ASilent: Boolean; const ALogFile: string);
+procedure SignFile(const AFileName, ASigntoolPath, ACertFile, ACertPass, ATimestamp, AHashAlgo: string;
+  const AEnableLog, ASilent: Boolean; const ALogFile: string; const ABasePath: string);
 var
   LExitCode: DWORD;
   LProcessInfo: TProcessInformation;
   LStartupInfo: TStartupInfo;
-  LFileHash, LRelName, LParams: string;
+  LFileHash: string;
 begin
-  LRelName := ToRelativePath(ABasePath, AFileName);
-
   if not FileNeedsSigning(AFileName, ALogFile) then
   begin
-    Log(LRelName + ' -> Skipped (already signed)', AEnableLog, ASilent, ALogFile);
+    Log(GetRelativePath(ABasePath, AFileName) + ' -> Skipped (already signed)', AEnableLog, ASilent, ALogFile);
     Exit;
   end;
 
   LFileHash := THashSHA2.GetHashString(AFileName);
-  LParams := Format('sign /f "%s" /p %s /tr "%s" /td %s /fd %s "%s"',
+  var LParams := Format(
+    'sign /f "%s" /p %s /tr "%s" /td %s /fd %s "%s"',
     [ACertFile, ACertPass, ATimestamp, AHashAlgo, AHashAlgo, AFileName]);
 
   FillChar(LStartupInfo, SizeOf(LStartupInfo), 0);
@@ -110,13 +101,15 @@ begin
     GetExitCodeProcess(LProcessInfo.hProcess, LExitCode);
     CloseHandle(LProcessInfo.hProcess);
     CloseHandle(LProcessInfo.hThread);
+
     if LExitCode = 0 then
-      Log(LRelName + ' -> OK [' + LFileHash + ']', AEnableLog, ASilent, ALogFile)
+      Log(GetRelativePath(ABasePath, AFileName) + ' -> OK [' + LFileHash + ']', AEnableLog, ASilent, ALogFile)
     else
-      Log(LRelName + Format(' -> Failed (ExitCode: %d) [' + LFileHash + ']', [LExitCode]), AEnableLog, ASilent, ALogFile);
+      Log(GetRelativePath(ABasePath, AFileName) + Format(' -> Failed (ExitCode: %d) [%s]', [LExitCode, LFileHash]),
+        AEnableLog, ASilent, ALogFile);
   end
   else
-    Log(LRelName + ' -> Failed to execute signtool: ' + SysErrorMessage(GetLastError) +
+    Log(GetRelativePath(ABasePath, AFileName) + ' -> Failed to execute signtool: ' + SysErrorMessage(GetLastError) +
       ' [' + LFileHash + ']', AEnableLog, ASilent, ALogFile);
 end;
 
@@ -129,16 +122,15 @@ begin
 end;
 
 procedure ParseParams(const ABasePath, AIniFileName: string;
-  out AEnableLog, ASilent, ARecursive: Boolean; out ALogFile: string; out ADirs: TArray<string>);
+  out AEnableLog, ASilent, ARecursive: Boolean; out ALogFile, ADirsOverride: string);
 begin
   AEnableLog := False;
   ASilent := False;
   ARecursive := False;
   ALogFile := '';
-  SetLength(ADirs, 0);
+  ADirsOverride := '';
 
   var LDefaultLog := TPath.Combine(ABasePath, ChangeFileExt(AIniFileName, '.log'));
-
   for var I := 1 to ParamCount do
   begin
     var LArg := ParamStr(I);
@@ -150,7 +142,7 @@ begin
     else if LArg.StartsWith('-log=', True) then
     begin
       AEnableLog := True;
-      ALogFile := Copy(LArg, 6, MaxInt);
+      ALogFile := LArg.Substring(5).Trim(['"', '''']);
       if not TPath.IsPathRooted(ALogFile) then
         ALogFile := TPath.Combine(ABasePath, ALogFile);
     end
@@ -159,45 +151,36 @@ begin
     else if SameText(LArg, '-allsubdirs') then
       ARecursive := True
     else if LArg.StartsWith('-directories=', True) then
-    begin
-      var LValue := Copy(LArg, 13, MaxInt);
-      ADirs := LValue.Split([';'], TStringSplitOptions.ExcludeEmpty);
-      for var J := 0 to High(ADirs) do
-        ADirs[J] := ResolvePath(ABasePath, ADirs[J]);
-    end;
+      ADirsOverride := LArg.Substring(12).Trim(['"', '''']);
   end;
 end;
 
-procedure ProcessFiles(const ATargetFolder, ASigntoolPath, ACertFile, ACertPass, ATimestamp, AHashAlgo, ABasePath: string;
+procedure ProcessFiles(const ATargetFolder, ASigntoolPath, ACertFile, ACertPass, ATimestamp, AHashAlgo: string;
   const AEnableLog, ASilent: Boolean; const ALogFile: string; ARecursive: Boolean;
-  const AExts: TArray<string>);
-var
-  LSearchOption: TSearchOption;
+  const AExts: TArray<string>; const ABasePath: string);
 begin
   if not TDirectory.Exists(ATargetFolder) then
   begin
-    Log('Directory not found: ' + ToRelativePath(ABasePath, ATargetFolder), AEnableLog, ASilent, ALogFile);
+    Log('Directory not found: ' + GetRelativePath(ABasePath, ATargetFolder), AEnableLog, ASilent, ALogFile);
     Exit;
   end;
 
+  var LSearchOption := TSearchOption.soTopDirectoryOnly;
   if ARecursive then
-    LSearchOption := TSearchOption.soAllDirectories
-  else
-    LSearchOption := TSearchOption.soTopDirectoryOnly;
+    LSearchOption := TSearchOption.soAllDirectories;
 
   for var LExt in AExts do
   begin
     var LValue := LExt.Trim;
     if LValue.IsEmpty then Continue;
     for var LFileName in TDirectory.GetFiles(ATargetFolder, '*.' + LValue, LSearchOption) do
-      SignFile(LFileName, ASigntoolPath, ACertFile, ACertPass, ATimestamp, AHashAlgo, ABasePath, AEnableLog, ASilent, ALogFile);
+      SignFile(LFileName, ASigntoolPath, ACertFile, ACertPass, ATimestamp, AHashAlgo, AEnableLog, ASilent, ALogFile, ABasePath);
   end;
 end;
 
 var
   LEnableLog, LSilent, LRecursive: Boolean;
-  LLogFile: string;
-  LDirs: TArray<string>;
+  LLogFile, LDirsOverride: string;
 begin
   try
     var LBasePath := IncludeTrailingPathDelimiter(ExtractFilePath(ParamStr(0)));
@@ -207,12 +190,13 @@ begin
     if not FileExists(LIniFullPath) then
       raise Exception.CreateFmt('Configuration file not found: %s', [LIniFullPath]);
 
-    ParseParams(LBasePath, LIniFileName, LEnableLog, LSilent, LRecursive, LLogFile, LDirs);
+    ParseParams(LBasePath, LIniFileName, LEnableLog, LSilent, LRecursive, LLogFile, LDirsOverride);
 
     var LIni := TIniFile.Create(LIniFullPath);
     try
       var LCertFile := ResolvePath(LBasePath, LIni.ReadString('Config', 'Certificate', ''));
       var LCertPass := LIni.ReadString('Config', 'Password', '');
+      var LDirList := LIni.ReadString('Config', 'Directories', '');
       var LExtList := LIni.ReadString('Config', 'Extensions', '');
       var LTimestamp := LIni.ReadString('Config', 'Timestamp', 'http://timestamp.digicert.com');
       var LHashAlgo := LIni.ReadString('Config', 'HashAlgorithm', 'SHA256');
@@ -230,18 +214,20 @@ begin
       if LExtList = '' then
         raise Exception.Create('No file extensions configured.');
 
-      if Length(LDirs) = 0 then
-      begin
-        var LDirList := LIni.ReadString('Config', 'Directories', '');
+      var LDirs: TArray<string>;
+      if LDirsOverride <> '' then
+        LDirs := LDirsOverride.Split([';'], TStringSplitOptions.ExcludeEmpty)
+      else
         LDirs := LDirList.Split([';'], TStringSplitOptions.ExcludeEmpty);
-        for var I := 0 to High(LDirs) do
-          LDirs[I] := ResolvePath(LBasePath, LDirs[I]);
-      end;
 
       var LExts := LExtList.Split([';'], TStringSplitOptions.ExcludeEmpty);
-      for var LDir in LDirs do
-        ProcessFiles(LDir, LSigntoolPath, LCertFile, LCertPass, LTimestamp, LHashAlgo, LBasePath, LEnableLog, LSilent, LLogFile, LRecursive, LExts);
 
+      for var LDir in LDirs do
+      begin
+        var LValue := ResolvePath(LBasePath, LDir);
+        ProcessFiles(LValue, LSigntoolPath, LCertFile, LCertPass, LTimestamp, LHashAlgo,
+          LEnableLog, LSilent, LLogFile, LRecursive, LExts, LBasePath);
+      end;
     finally
       LIni.Free;
     end;
